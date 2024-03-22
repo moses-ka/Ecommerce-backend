dc1e5b6a212dd694295e542c714253040acb177d
Moses <ji.moses.ka@gmail.com>
2024-03-22 19:41:20 +0100

    added size and color to product

--- EcommerceApi/myapp/models.py
+++ EcommerceApi/myapp/models.py
@@ -3,13 +3,31 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
+    sex_choices = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('U', 'Unisex'),
+    ]
+    color_choices = [
+        ('Black', 'Black'),
+        ('White', 'White'),
+    ]
+    size_choices = [
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+        ('XL', 'Extra Large'),
+    ]
+        
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(decimal_places=2, max_digits=10000)
-    sex = models.CharField(max_length=120)
+    sex = models.CharField(max_length=1, choices=sex_choices,null=True, blank=True)
     img = models.ImageField(blank=True, null=True)
-    size = models.CharField(max_length=120)
+    size = models.CharField(max_length=2 ,choices=size_choices ,null=True, blank=True)
     tags = models.CharField(max_length=120,null=True, blank=True)
+    color = models.CharField(max_length=120, choices=color_choices ,null=True, blank=True)
+    
     def __str__(self):
         return self.title
 
